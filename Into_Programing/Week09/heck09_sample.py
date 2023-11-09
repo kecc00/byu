dc1576a1b1184ebc7b8da4fe7c52c2fddd18e11b
Kevin Cruz <kevin.cruz.carcamo@gmail.com>
2023-11-08 21:13:13 -0600

    primer commit

---
+++ Into_Programing/Week09/heck09_sample.py
@@ -0,0 +1,21 @@
+"""
+File: check09_sample.py
+Author: Kevin E. Cruz
+"""
+
+
+friend_list = []
+
+#
+while True: 
+    friend_name = input("Enter a friend's name or type 'finish' to end: ")
+
+    if friend_name.lower() == 'finish':
+        break
+
+    if friend_name.lower() != 'finish':
+        friend_list.append(friend_name)
+
+print("List of friends:")
+for friend in friend_list:
+    print(friend)
